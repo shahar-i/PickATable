4edcefa975ea6328d145c35650ca880b71b06a9e
shahar <shahar@shahar-laptop>
2016-10-26 23:11:04 +0300

    הכנסת יוזר לדאטה בייס

--- new_waiter.php
+++ new_waiter.php
@@ -1,13 +1,14 @@
 <html>
     <head>  <link rel="stylesheet" type="text/css" href="menu.css"></head>
-    <body>
+    <body >
         <h1>רישום מלצר חדש</h1>
-        
-        <form action="new_waiter.php" method="POST">
+      
+        <form  action="new_waiter.php" method="POST">
           <input id="name" type="text" placeholder="Username" name="username">
           <input id="name" type="password" placeholder="Password" name="password">
     <input  class="Password_Recovery_Ok"  type="Submit"  name="Submit" value="אישור" /></br></br>
-        </form>
+       
+        </form> 
     </body>
 </html>
 
@@ -17,23 +18,39 @@
     
 
     
-    if (isset($_POST['Submit']))//בדיקה אם מה שנבחר נמצא במערך הקינוחים
+    if (isset($_POST['Submit']))
         { 
         
         $con=mysql_connect('localhost','root','');
 mysql_select_db('user',$con);
 
+    $username1 = $_POST['username']; 
+    
+    
+    $result = mysql_query("SELECT * FROM users WHERE username='$username'");
+    
+     if (mysql_num_rows($result)) {
+while ($name_db = mysql_fetch_array($result)){
+   
+       
 
     
-    $named=$_POST["username"];
-    $price=$_POST["password"];
-    
-    
-    
-            mysql_query("INSERT INTO `user`.`users` (`username`, `last_name`, `password`, `mail`) VALUES ('$named', '', '$price', '');");//מכניס לטבלת הזמנות מה שנבחר 
-            
+
+    $username1 = $_POST['username'];    
+    $username2 = $name_db['username'];
+
+    if ($username1 == $username2 ) {
+       echo 'not found';
+       break;
+    }
+ else {
+            $_SESSION['username'] = $res['username'];
+                 mysql_query("INSERT INTO `user`.`users` (`username`, `last_name`,`password`, `mail`) VALUES ('$username1', '', '', '');");//מכניס לטבלת הזמנות מה שנבחר 
+
+    }      
+    }    
             
         } 
-    
+        }    
 ?>
 
