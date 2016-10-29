ec143921c1d66573dbf7c4233187c4f1e3444031
daniel <daniel@DESKTOP-4QJHJS1>
2016-10-29 16:15:04 +0300

    Desserts.php

---
+++ edit_user.php
@@ -0,0 +1,28 @@
+<html>
+    <head>  <link rel="stylesheet" type="text/css" href="menu.css"></head>
+    <body >
+        <h1>עדכון פרטי מלצר</h1>
+      
+        <form  action="new_waiter.php" method="POST">
+          <input id="name" type="text" placeholder="Username" name="username">
+          <input id="name" type="password" placeholder="Password" name="password">
+          <input  class=""  type="submit"  name="Submit" value="אישור" /></br></br>
+       
+        </form> 
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+?>
+
