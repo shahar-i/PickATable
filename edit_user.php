3047c640e2f5e47fcd3d05fa43e0ce79ac6ebf36
shahar <shahar@shahar-laptop>
2016-11-03 00:43:09 +0200

    שינוי יוזרים

--- edit_user.php
+++ edit_user.php
@@ -2,12 +2,13 @@
     <head>  <link rel="stylesheet" type="text/css" href="menu.css"></head>
     <body >
         <h1>עדכון פרטי מלצר</h1>
-      
-        <form  action="new_waiter.php" method="POST">
-          <input id="name" type="text" placeholder="Username" name="username">
-          <input id="name" type="password" placeholder="Password" name="password">
-          <input  class=""  type="submit"  name="Submit" value="אישור" /></br></br>
-       
+
+        <form  action="edit_user.php" method="POST">
+            <input id="name" type="text" placeholder="Username" name="username">
+             <input id="name" type="text" placeholder="New Username" name="new_username">
+             <input id="name" type="password" placeholder="Password" name="password">
+            <input  class=""  type="submit"  name="Submit" value="אישור" /></br></br>
+
         </form> 
     </body>
 </html>
@@ -17,12 +18,49 @@
 
 
 
-
-
-
-
-
 <?php
+if (isset($_POST['Submit'])) {
 
+    $con = mysql_connect('localhost', 'root', '');
+    mysql_select_db('user', $con);
+
+
+
+
+
+    $username1 = $_POST['username'];
+
+
+    $result5 = mysql_query("SELECT * FROM `users`");
+
+    $i = 1;
+    while ($name_db = mysql_fetch_array($result5)) {
+
+
+
+        $username1 = $_POST['username'];
+        $username2 = $name_db['username'];
+        $new_user = $_POST['new_username'];
+        $email = $_POST['email'];
+        $last_name = $_POST['last_name'];
+        $password = $_POST['password'];
+
+
+        if ($username1 == $username2) {
+            echo 'kayam';
+            $i = 0;
+            if(isset($_POST[$username1])==$username2)
+            $new_user=$username1;
+                    mysql_query("UPDATE users SET username='$new_user' WHERE username='$username2';");
+
+            break;
+        }
+    }
+
+    if ($i == 1) {
+        echo 'creart a new user';
+        $_SESSION['username'] = $res['username'];
+    }
+}
 ?>
 
--- new_waiter.php
+++ new_waiter.php
@@ -2,63 +2,55 @@
     <head>  <link rel="stylesheet" type="text/css" href="menu.css"></head>
     <body >
         <h1>רישום מלצר חדש</h1>
-      
+
         <form  action="new_waiter.php" method="POST">
-          <input id="name" type="text" placeholder="Username" name="username">
-          <input id="name" type="password" placeholder="Password" name="password">
-          <input  class=""  type="submit"  name="Submit" value="אישור" /></br></br>
-       
+            <input id="name" type="text" placeholder="Username" name="username">
+            <input id="name" type="password" placeholder="Password" name="password">
+            <input id="name" type="email" placeholder="email" name="email">
+            <input id="name" type="text" placeholder="Last name" name="last_name">
+
+            <input  class=""  type="submit"  name="Submit" value="אישור" /></br></br>
+
         </form> 
     </body>
 </html>
 
 <?php
+if (isset($_POST['Submit'])) {
+
+    $con = mysql_connect('localhost', 'root', '');
+    mysql_select_db('user', $con);
 
 
-    
+    $username1 = $_POST['username'];
 
-    
-    if (isset($_POST['Submit']))
-        { 
-       
-        $con=mysql_connect('localhost','root','');
-mysql_select_db('user',$con);
- 
-  
-    $username1 = $_POST['username']; 
-      
-    
+
     $result5 = mysql_query("SELECT * FROM `users`");
-    
-    $i=1;
-     while ($name_db = mysql_fetch_array($result5)) { 
-       
-     
 
-    $username1 = $_POST['username'];    
-    $username2 = $name_db['username'];
-    
-   
-    
-    $password = $_POST['password'];  
-    
-     
-    if ($username1 == $username2) {
-       echo 'kayam';
-       $i=0;
-       break;
-       
+    $i = 1;
+    while ($name_db = mysql_fetch_array($result5)) {
+
+
+
+        $username1 = $_POST['username'];
+        $username2 = $name_db['username'];
+        $email = $_POST['email'];
+        $last_name = $_POST['last_name'];
+        $password = $_POST['password'];
+
+
+        if ($username1 == $username2) {
+            echo 'kayam';
+            $i = 0;
+            break;
+        }
     }
-         
-     } 
-       
-     if($i==1)
-     {
-        echo 'not found,nichnas';
-            $_SESSION['username'] = $res['username'];
-                 mysql_query("INSERT INTO `user`.`users` (`username`, `last_name`,`password`, `mail`) VALUES ('$username1', '', '$password', '');"); 
-     }
-        } 
-           
+
+    if ($i == 1) {
+        echo 'creart a new user';
+        $_SESSION['username'] = $res['username'];
+        mysql_query("INSERT INTO `user`.`users` (`username`, `last_name`,`password`, `mail`) VALUES ('$username1', '$last_name', '$password', '$email');");
+    }
+}
 ?>
 
